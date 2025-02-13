5fd677bca688aa6369f5a30d20ddecd89a7dfe6d
Shikha Gupta <shikha912513@gmail.com>
2025-02-13 19:16:43 +0530

    project added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,281 +1,8 @@
-// import {
-//   mobile,
-//   backend,
-//   creator,
-//   web,
-//   javascript,
-//   typescript,
-//   html,
-//   css,
-//   reactjs,
-//   redux,
-//   tailwind,
-//   nodejs,
-//   mongodb,
-//   git,
-//   figma,
-//   docker,
-//   meta,
-//   //starbucks,
-//   nitj,
-//   tesla,
-//   shopify,
-//   //carrent,
-//   jobportal,
-//   jobit,
-//   tripguide,
-//   threejs,
-// } from "../assets";
-
-// export const navLinks = [
-//   {
-//     id: "about",
-//     title: "About",
-//   },
-//   {
-//     id: "work",
-//     title: "Work",
-//   },
-//   {
-//     id: "contact",
-//     title: "Contact",
-//   },
-// ];
-
-// const services = [
-//   {
-//     title: "Web Developer",
-//     icon: web,
-//   },
-//   {
-//     title: "React Native Developer",
-//     icon: mobile,
-//   },
-//   {
-//     title: "Backend Developer",
-//     icon: backend,
-//   },
-//   // {
-//   //   title: "Content Creator",
-//   //   icon: creator,
-//   // },
-// ];
-
-// const technologies = [
-//   {
-//     name: "HTML 5",
-//     icon: html,
-//   },
-//   {
-//     name: "CSS 3",
-//     icon: css,
-//   },
-//   {
-//     name: "JavaScript",
-//     icon: javascript,
-//   },
-//   // {
-//   //   name: "TypeScript",
-//   //   icon: typescript,
-//   // },
-//   {
-//     name: "React JS",
-//     icon: reactjs,
-//   },
-//   {
-//     name: "Redux Toolkit",
-//     icon: redux,
-//   },
-//   {
-//     name: "Tailwind CSS",
-//     icon: tailwind,
-//   },
-//   {
-//     name: "Node JS",
-//     icon: nodejs,
-//   },
-//   {
-//     name: "MongoDB",
-//     icon: mongodb,
-//   },
-//   {
-//     name: "Three JS",
-//     icon: threejs,
-//   },
-//   {
-//     name: "git",
-//     icon: git,
-//   },
-//   // {
-//   //   name: "figma",
-//   //   icon: figma,
-//   // },
-//   // {
-//   //   name: "docker",
-//   //   icon: docker,
-//   // },
-// ];
-
-// const experiences = [
-//   {
-//     // title: "React.js Developer",
-//     // company_name: "Starbucks",
-//     // icon: starbucks,
-//     // iconBg: "#383E56",
-//     // date: "March 2020 - April 2021",
-//     // points: [
-//     //   "Developing and maintaining web applications using React.js and other related technologies.",
-//     //   "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-//     //   "Implementing responsive design and ensuring cross-browser compatibility.",
-//     //   "Participating in code reviews and providing constructive feedback to other developers.",
-//     // ],
-    
-//       title: "Front-End Developer",
-//       company_name: "NIT Jalandhar",
-//       icon: nitj, // Replace with the appropriate icon
-//       iconBg: "#383E56",
-//       date: "March 2023 - Nov 2024",
-//       points: [
-//           "Developed front-end for over 10 pages in the Administration section of the Institute’s website.",
-//           "Worked primarily with HTML, Tailwind CSS, and JavaScript to build responsive and user-friendly interfaces.",
-//           "Collaborated with team members to enhance the website's functionality and design.",
-//           "Ensured cross-browser compatibility and optimized website performance.",
-//       ],
-  
-  
-//   },
-//   {
-//     // title: "React Native Developer",
-//     // company_name: "Tesla",
-//     // icon: tesla,
-//     // iconBg: "#E6DEDD",
-//     // date: "Jan 2021 - Feb 2022",
-//     // points: [
-//     //   "Developing and maintaining web applications using React.js and other related technologies.",
-//     //   "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-//     //   "Implementing responsive design and ensuring cross-browser compatibility.",
-//     //   "Participating in code reviews and providing constructive feedback to other developers.",
-//     // ],
-    
-//       title: "Full-Stack Developer",
-//       company_name: "Cognifyz",
-//       icon: shopify, // Replace with the appropriate icon
-//       iconBg: "#383E56",
-//       date: "Summer 2024",
-//       points: [
-//           "Worked on two major projects: a password manager and a text-based game.",
-//           "Developed the backend and frontend for the password manager, ensuring security features like password validation and encryption.",
-//           "Created an engaging text-based game with dynamic user interaction and scoring features.",
-//           "Collaborated with the team to integrate various tools and libraries for enhanced functionality and performance.",
-//           "Gained hands-on experience with both front-end and back-end technologies, including React, Node.js, Express, and MongoDB."
-//       ],
-  
-  
-//   },
-// ];
-
-// const testimonials = [
-//   {
-//     testimonial:
-//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-//     name: "Sara Lee",
-//     designation: "CFO",
-//     company: "Acme Co",
-//     image: "https://randomuser.me/api/portraits/women/4.jpg",
-//   },
-//   {
-//     testimonial:
-//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-//     name: "Chris Brown",
-//     designation: "COO",
-//     company: "DEF Corp",
-//     image: "https://randomuser.me/api/portraits/men/5.jpg",
-//   },
-//   {
-//     testimonial:
-//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-//     name: "Lisa Wang",
-//     designation: "CTO",
-//     company: "456 Enterprises",
-//     image: "https://randomuser.me/api/portraits/women/6.jpg",
-//   },
-// ];
-
-// const projects = [
-//   {
-//     name: "Job Portal",
-//     description:
-//       " Developed CareerBloom, a full-stack job portal designed to seamlessly connect job seekers and recruiters. The motivation behind this project was to bridge the gap between talent and opportunity",
-//     tags: [
-//       {
-//         name: "react",
-//         color: "blue-text-gradient",
-//       },
-//       {
-//         name: "mongodb",
-//         color: "green-text-gradient",
-//       },
-//       {
-//         name: "tailwind",
-//         color: "pink-text-gradient",
-//       },
-//     ],
-//     image: "https://cdn.phpjabbers.com/files/templates_img/template133-list.jpg",
-//     source_code_link: "https://job-linker-xoes.onrender.com/",
-//   },
-//   {
-//     name: "e-Aarogya",
-//     description:
-//       "Developed e-Arogya, an online doctor appointment system, as a full-stack web application to address the challenges faced in accessing healthcare in rural area.",
-//     tags: [
-//       {
-//         name: "react",
-//         color: "blue-text-gradient",
-//       },
-//       {
-//         name: "mondodb",
-//         color: "green-text-gradient",
-//       },
-//       {
-//         name: "Express.js",
-//         color: "pink-text-gradient",
-//       },
-//     ],
-//     image: "https://hrbanana.com/wp-content/uploads/2020/12/book-your-medical-appointment-online-using-mobile-app-doctor-sitting-desk-background-wearing-face-mask-190595834.jpg",
-//     source_code_link: "https://bookmydoc-six.vercel.app/login",
-//   },
-//   {
-//     name: "BookStore",
-//     description:
-//       "Developed a bookstore app that allows users to search for and purchase books, featuring both free and paid content",
-//     tags: [
-//       {
-//         name: "react",
-//         color: "blue-text-gradient",
-//       },
-//       {
-//         name: "tailwind",
-//         color: "green-text-gradient",
-//       },
-//       {
-//         name: "mongodb",
-//         color: "pink-text-gradient",
-//       },
-//     ],
-//     image: "https://png.pngtree.com/thumb_back/fw800/background/20230120/pngtree-bookstore-bookshelf-display-photography-image_1534440.jpg",
-//     source_code_link: "https://github.com/Shikha-9125/bookstoreApp",
-//   },
-// ];
-
-// export { services, technologies, experiences, testimonials,projects };
-
 import {
   mobile,
   backend,
-  creator,
   web,
   javascript,
-  typescript,
   html,
   css,
   reactjs,
@@ -284,18 +11,8 @@ import {
   nodejs,
   mongodb,
   git,
-  figma,
-  docker,
-  meta,
-  //starbucks,
-  nitj,
-  tesla,
-  shopify,
-  //carrent,
-  jobportal,
-  jobit,
-  tripguide,
   threejs,
+  nitj,
 } from "../assets";
 
 export const navLinks = [
@@ -326,10 +43,6 @@ const services = [
     title: "Backend Developer",
     icon: backend,
   },
-  // {
-  //   title: "Content Creator",
-  //   icon: creator,
-  // },
 ];
 
 const technologies = [
@@ -345,10 +58,6 @@ const technologies = [
     name: "JavaScript",
     icon: javascript,
   },
-  // {
-  //   name: "TypeScript",
-  //   icon: typescript,
-  // },
   {
     name: "React JS",
     icon: reactjs,
@@ -377,33 +86,13 @@ const technologies = [
     name: "git",
     icon: git,
   },
-  // {
-  //   name: "figma",
-  //   icon: figma,
-  // },
-  // {
-  //   name: "docker",
-  //   icon: docker,
-  // },
 ];
 
 const experiences = [
   {
-    // title: "React.js Developer",
-    // company_name: "Starbucks",
-    // icon: starbucks,
-    // iconBg: "#383E56",
-    // date: "March 2020 - April 2021",
-    // points: [
-    //   "Developing and maintaining web applications using React.js and other related technologies.",
-    //   "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    //   "Implementing responsive design and ensuring cross-browser compatibility.",
-    //   "Participating in code reviews and providing constructive feedback to other developers.",
-    // ],
-    
     title: "Front-End Developer",
     company_name: "NIT Jalandhar",
-    icon: nitj, // Replace with the appropriate icon
+    icon: nitj,
     iconBg: "#383E56",
     date: "March 2023 - Nov 2024",
     points: [
@@ -414,21 +103,9 @@ const experiences = [
     ],
   },
   {
-    // title: "React Native Developer",
-    // company_name: "Tesla",
-    // icon: tesla,
-    // iconBg: "#E6DEDD",
-    // date: "Jan 2021 - Feb 2022",
-    // points: [
-    //   "Developing and maintaining web applications using React.js and other related technologies.",
-    //   "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    //   "Implementing responsive design and ensuring cross-browser compatibility.",
-    //   "Participating in code reviews and providing constructive feedback to other developers.",
-    // ],
-    
     title: "Full-Stack Developer",
     company_name: "Cognifyz",
-    icon: "https://cognifyz.com/wp-content/uploads/2023/11/cognifyz-1.png", // Replace with the appropriate icon
+    icon: "https://cognifyz.com/wp-content/uploads/2023/11/cognifyz-1.png",
     iconBg: "#383E56",
     date: "Summer 2024",
     points: [
@@ -436,29 +113,34 @@ const experiences = [
       "Developed the backend and frontend for the password manager, ensuring security features like password validation and encryption.",
       "Created an engaging text-based game with dynamic user interaction and scoring features.",
       "Collaborated with the team to integrate various tools and libraries for enhanced functionality and performance.",
-      "Gained hands-on experience with both front-end and back-end technologies, including React, Node.js, Express, and MongoDB."
+      "Gained hands-on experience with both front-end and back-end technologies, including React, Node.js, Express, and MongoDB.",
     ],
   },
 ];
 
 const projects = [
   {
+    name: "Realtime Collaborative IDE",
+    description:
+      "Developed an online IDE enabling real-time code collaboration, allowing multiple users to edit the same file simultaneously. The platform includes code compilation, input handling (stdin), syntax highlighting, user authentication, and role-based access control.",
+    tags: [
+      { name: "react", color: "blue-text-gradient" },
+      { name: "node.js", color: "green-text-gradient" },
+      { name: "mongodb", color: "pink-text-gradient" },
+      { name: "webRTC", color: "yellow-text-gradient" },
+      { name: "Yjs", color: "grey-text-gradient" },
+    ],
+    image: "https://discuss.yjs.dev/uploads/default/original/1X/8c8a1961aaa1fa50aa240e81a98e733d0900f99f.jpeg",
+    source_code_link: "https://github.com/Shikha-9125/RealtimeIDE",
+  },
+  {
     name: "Job Portal",
     description:
-      " Developed CareerBloom, a full-stack job portal designed to seamlessly connect job seekers and recruiters. The motivation behind this project was to bridge the gap between talent and opportunity",
+      "Developed CareerBloom, a full-stack job portal designed to seamlessly connect job seekers and recruiters. The motivation behind this project was to bridge the gap between talent and opportunity.",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
+      { name: "react", color: "blue-text-gradient" },
+      { name: "mongodb", color: "green-text-gradient" },
+      { name: "tailwind", color: "pink-text-gradient" },
     ],
     image: "https://cdn.phpjabbers.com/files/templates_img/template133-list.jpg",
     source_code_link: "https://job-linker-xoes.onrender.com/",
@@ -466,20 +148,11 @@ const projects = [
   {
     name: "e-Aarogya",
     description:
-      "Developed e-Arogya, an online doctor appointment system, as a full-stack web application to address the challenges faced in accessing healthcare in rural area.",
+      "Developed e-Arogya, an online doctor appointment system, as a full-stack web application to address the challenges faced in accessing healthcare in rural areas.",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mondodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Express.js",
-        color: "pink-text-gradient",
-      },
+      { name: "react", color: "blue-text-gradient" },
+      { name: "mongodb", color: "green-text-gradient" },
+      { name: "Express.js", color: "pink-text-gradient" },
     ],
     image: "https://hrbanana.com/wp-content/uploads/2020/12/book-your-medical-appointment-online-using-mobile-app-doctor-sitting-desk-background-wearing-face-mask-190595834.jpg",
     source_code_link: "https://bookmydoc-six.vercel.app/login",
@@ -487,20 +160,11 @@ const projects = [
   {
     name: "BookStore",
     description:
-      "Developed a bookstore app that allows users to search for and purchase books, featuring both free and paid content",
+      "Developed a bookstore app that allows users to search for and purchase books, featuring both free and paid content.",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "green-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "pink-text-gradient",
-      },
+      { name: "react", color: "blue-text-gradient" },
+      { name: "tailwind", color: "green-text-gradient" },
+      { name: "mongodb", color: "pink-text-gradient" },
     ],
     image: "https://png.pngtree.com/thumb_back/fw800/background/20230120/pngtree-bookstore-bookshelf-display-photography-image_1534440.jpg",
     source_code_link: "https://github.com/Shikha-9125/bookstoreApp",
@@ -508,3 +172,4 @@ const projects = [
 ];
 
 export { services, technologies, experiences, projects };
+
